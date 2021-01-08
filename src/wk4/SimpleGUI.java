21dac4b8c52023835b031c87f4a0548867cf5a44
Chris Taylor <gitlab@taylor.bz>
2021-01-08 07:11:50 -0600

    Initial commit with event handler in stand-alone class

---
+++ src/wk4/SimpleGUI.java
@@ -0,0 +1,27 @@
+package wk4;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class SimpleGUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Pane root = new StackPane();
+        Button clickMe = new Button("Click Me");
+        clickMe.setOnAction(new ClickMeHandler());
+        root.getChildren().add(clickMe);
+
+        primaryStage.setTitle("Simple GUI");
+        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.show();
+    }
+}
