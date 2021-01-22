db7af69f127ef503de17ba85f0ca8266d220093a
Chris Taylor <gitlab@taylor.bz>
2021-01-22 09:53:28 -0600

    Lecture 3 from week 6.

---
+++ src/wk6/FileIO.java
@@ -0,0 +1,41 @@
+package wk6;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FileIO {
+    public static void main(String[] args) throws FileNotFoundException {
+        List<String> lines = new ArrayList<>();
+        lines.add("fdghjkdidud");
+        lines.add("dghjkdidud");
+        lines.add("fghjkdidud");
+        lines.add("fdhjkdidud");
+        writeTextFile("example.txt", lines);
+    }
+
+    public static void writeTextFile(String filename, List<String> lines) throws FileNotFoundException {
+        try (PrintWriter out = new PrintWriter(filename)) {
+            for(String line : lines) {
+                out.println(line);
+            }
+        }
+    }
+
+    public static List<String> readTextFile(String filename) {
+        List<String> lines = new ArrayList<>();
+        try (InputStream istream = Files.newInputStream(Paths.get(filename);
+             Scanner in = new Scanner(istream)) {
+            while(in.hasNextLine()) {
+                lines.add(in.nextLine());
+            }
+        }
+        return lines;
+    }
+}
