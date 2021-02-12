4f60e9c07335d0cd27ada571ed098ab0838535b0
Chris Taylor <gitlab@taylor.bz>
2021-02-12 08:54:58 -0600

    Covid exercise at end of class

--- src/wk9/covid/Covid.java
+++ src/wk9/covid/Covid.java
@@ -6,6 +6,7 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -17,12 +18,35 @@ public class Covid {
     public static void main(String[] args) {
         try {
             Covid covid = new Covid(URL);
-            System.out.println(covid.getTotalEntries());
+            System.out.println(covid.getPercentagePositivePCR());
+            System.out.println(covid.questions());
         } catch (MalformedURLException e) {
             System.err.println("Invalid URL");
         } catch (IOException e) {
             System.err.println("Error reading file");
         }
+    }
+
+    private String questions() {
+        Optional<Entry> badDayInState = entries.parallelStream()
+                .filter(e -> e.onVentilatorCurrently.isPresent())
+                .max((e, f) -> e.onVentilatorCurrently.getAsInt()-f.onVentilatorCurrently.getAsInt());
+        Entry bdis = badDayInState.orElse(null);
+        return bdis!=null ? bdis.state : "";
+    }
+
+    private double getPercentagePositivePCR() {
+        return 100.0 * entries.stream()
+                .filter(e -> e.date.equals(entries.get(0).date))
+                .filter(e -> e.positiveCasesViral.isPresent())
+                .mapToInt(e -> e.positiveCasesViral.getAsInt())
+                .sum()
+                /
+                entries.stream()
+                .filter(e -> e.date.equals(entries.get(0).date))
+                .filter(e -> e.totalTestsViral.isPresent())
+                .mapToInt(e -> e.totalTestsViral.getAsInt())
+                .sum() ;
     }
 
     private int getTotalEntries() {
